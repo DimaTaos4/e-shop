ad2165dd231209ae82efa16b0f4d9a1612c370ee
Dima <taominhquan2004@gmail.com>
2025-05-01 09:24:57 +0200

    fix: static.json and vite config all

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // <-- добавь это, если сайт не загружается
+  base: './',
   preview: {
     host: true,
     port: 3000,
-    allowedHosts: ['e-shop-5-9782.onrender.com']
+    allowedHosts: ['all'] // Разрешаем все хосты (не нужно добавлять каждый новый)
   }
 })
