070ecf350d8a9776a2c3ee578635cf9d2c8801de
Dima <taominhquan2004@gmail.com>
2025-05-01 09:08:13 +0200

    fixed all

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 3000,
+    allowedHosts: ['e-shop-4-ntw9.onrender.com']
+  }
 })
