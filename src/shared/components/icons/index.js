88356622bb6ff1fd4791350c7cb8af0308100e76
Dima <taominhquan2004@gmail.com>
2025-05-01 11:40:03 +0200

    fix export Facebook icon

--- src/shared/components/icons/index.js
+++ src/shared/components/icons/index.js
@@ -1,5 +1,5 @@
 
-export { default as FacebookIcon } from './FaceBookIcon'
+export { default as FacebookIcon } from './FacebookIcon';
 export { default as TwitterIcon } from './TwitterIcon';
 export { default as InstagramIcon } from './InstagramIcon';
 
